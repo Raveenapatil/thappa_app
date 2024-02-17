e4c374e2be54c3b0498f514fbdcfabab400620aa
raveena <raveenapatil4@gmail.com>
2024-02-17 10:24:56 +0530

    new

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+var React = require('react');
+var ReactDOM = require('react-dom');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(<h1>hhhihih</h1>,document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// ReactDOM.render(
+//   React.createElement("h1",null,"THappa Like"),
+//   document.getElementById("root")
+// );
+
+// var h1 = document.createElement("h1");
+// h1.innerHTML="thappa sub";
+// document.getElementById("root").appendChild(h1);
